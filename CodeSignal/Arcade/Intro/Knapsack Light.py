413701fa646be0bf68189a98777f258be6690ee3
camiloricoe <camilo.rico.echeverry@gmail.com>
2022-12-21 13:52:34 -0500

    Knapsack

--- CodeSignal/Arcade/Intro/Knapsack Light.py
+++ CodeSignal/Arcade/Intro/Knapsack Light.py
@@ -12,6 +12,20 @@ def solution(value1, weight1, value2, weight2, maxW):
         return value1
     else:    
         return 0
+
+
+def morral(tamano,pesos,valores,n):
+    if n ==0 or tamano ==0:
+        return 0
+    if pesos[n-1]>tamano:
+        return morral(tamano,pesos,valores,n-1)
+
+    return max(valores[n-1]+morral(tamano-pesos[n-1], pesos, valores, n-1), morral(tamano, pesos, valores, n-1))
+
+
+def solution(value1, weight1, value2, weight2, maxW):
+    return morral(maxW,[weight1,weight2],[value1,value2],2)
+
 """
       
 def solution(value1, weight1, value2, weight2, maxW):
