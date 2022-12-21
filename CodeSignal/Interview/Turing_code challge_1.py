ad0ecd512d19dcd8ebac8f8ab7d84a96dab87cd1
camiloricoe <camilo.rico.echeverry@gmail.com>
2022-12-21 13:00:02 -0500

    Turing.com code challenge open AI

--- CodeSignal/Interview/Turing_code challge_1.py
+++ CodeSignal/Interview/Turing_code challge_1.py
@@ -17,3 +17,37 @@ for i in bien:
        break
 
 print(res)
+
+#open AI code: 
+"""
+create a python program that recives a list with a ordered list but one numer of the list is repeted. the function should return the number that is repeted and the number that is missing. 
+
+for expample it recives [1,2,3,4,3]
+it needs to return [3, 5]
+
+"""
+
+def find_duplicate_and_missing(numbers):
+  # Calculate the sum of the numbers and the sum of the squares
+  sum_of_numbers = sum(numbers)
+  sum_of_squares = sum([x ** 2 for x in numbers])
+
+  # Calculate the expected sum of the numbers and the expected sum of the squares
+  expected_sum = len(numbers) * (len(numbers) + 1) // 2
+  expected_squares_sum = (2 * len(numbers) + 1) * \
+      (len(numbers) + 1) * (len(numbers)) // 6
+
+  # Calculate the difference between the expected and actual sums
+  difference = sum_of_numbers - expected_sum
+  squares_difference = sum_of_squares - expected_squares_sum
+
+  # Calculate the duplicate and missing numbers
+  duplicate = (difference + squares_difference // difference) // 2
+  missing = duplicate - difference
+
+  return [duplicate, missing]
+
+
+# Test the function
+print(find_duplicate_and_missing(nums))
+
