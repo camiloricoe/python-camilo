59640ee579a47df19850e02b495700ea6b66b8c8
camiloricoe <camilo.rico.echeverry@gmail.com>
2023-01-06 09:20:15 -0500

    Book Market part1

---
+++ CodeSignal/Arcade/The Core/53-Is Tandem Repeat?.py
@@ -0,0 +1,2 @@
+def solution(inputString):
+    return inputString[:len(inputString)//2]== inputString[len(inputString)//2:]
---
+++ CodeSignal/Arcade/The Core/54-Is Case-Insensitive Palindrome?.py
@@ -0,0 +1,3 @@
+def solution(inputString):
+    a=inputString.lower()
+    return a==a[::-1]
