ff1a6019acc379f8cb958a56b0c35c599bee55f3
camiloricoe <camilo.rico.echeverry@gmail.com>
2023-01-09 09:01:45 -0500

    Add new solution Election winners and are Similar

--- CodeSignal/Arcade/Intro/16-Are Similar.py
+++ CodeSignal/Arcade/Intro/16-Are Similar.py
@@ -1,2 +1,6 @@
 def solution(A, B):
     return sorted(A)==sorted(B) and sum([a!=b for a,b in zip(A,B)])<=2
+
+def solution(A, B):
+    d = [(x,y) for x,y in zip(A,B) if x!=y]
+    return len(d) == 0 or (len(d) == 2 and d[0][::-1]==d[1])
--- CodeSignal/Arcade/Intro/46-Elections Winners.py
+++ CodeSignal/Arcade/Intro/46-Elections Winners.py
@@ -7,3 +7,10 @@ def solution(votes, k):
         if i+k > maxBeforeCast:
             possible += 1
     return possible
+
+
+def solution(votes, k):
+    m = max(votes)
+    if k == 0:
+        return 1 if len([x for x in votes if x == m]) == 1 else 0
+    return len([x for x in votes if x+k > m])
