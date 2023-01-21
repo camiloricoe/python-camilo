4e0bbc642a5895b6ce428a5c5601d51928ed4504
camiloricoe <camilo.rico.echeverry@gmail.com>
2023-01-21 13:16:57 -0500

    refactor mainapp

--- Platzi/FastAPI 1.0/main.py
+++ Platzi/FastAPI 1.0/main.py
@@ -11,8 +11,8 @@ app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
 app.version ="0.0.1"
 
-
 Base.metadata.create_all(bind=engine)
+
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
@@ -76,66 +76,29 @@ def login(user: User):
     return JSONResponse(content=token, status_code=200)
 
 
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+def get_movies() -> List[Movie]:
+    return JSONResponse(status_code=200,content=movies )
 
 
-#metodo get
-
-@app.get('/', tags=['Home'])
-def message():
-    return HTMLResponse('<h1 style=color:red> Hola Mundo </h1>')
-
-
-@app.get('/movies', tags=['movies'])
-def get_movies():
-    return movies
-
-
-#parametro de ruta
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int =Path(ge=1,le=10000)):
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie, status_code=200)
+def get_movie(id: int = Path(ge=1, le=10000)) -> Movie:
     for movie in movies:
-        if movie['id']==id:
-            return movie
-    return []
+        if movie['id'] == id:
+            return JSONResponse(status_code=200, content=movie)
+    return JSONResponse(status_code=404, content=[])
 
-#parametros Query ... clave y valor
+# parametros Query ... clave y valor
 @app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category: str =Query(min_length=5, max_length=15)):
-    return [movie for movie in movies if movie['category'] == category]
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15),status_code = 200):
+    data = [movie for movie in movies if movie['category'] == category]
+    return JSONResponse(status_code=200, content=data)
 
-
-# #metodo post
-# @app.post('/movies', tags=['movies'])
-# def craete_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating:float = Body(), category:str = Body()):
-#     movies.append({
-#         'id': id,
-#         'title': title,
-#         'overview': overview,
-#         'year': year,
-#         'rating': rating,
-#         'category': category
-#     })
-#     return movies
-
-
-# metodo post con el esquema
-@app.post('/movies', tags=['movies'])
-def create_movie(movie: Movie):
+@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
+# def create_movie(movie: Movie): #funciona igual
+def create_movie(movie: Movie) -> dict:
     movies.append(movie)
-    return movies
-
-# # put
-# @app.put('/movies/{id}', tags=['movies'])
-# def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
-#     for item in movies:
-#         if item['id'] == id:
-#             item['title'] = title
-#             item['overview'] = overview
-#             item['year'] = year
-#             item['rating'] = rating
-#             item['category'] = category
-#             return movies
-
+    return JSONResponse(status_code=201, content={"message": "Se ha registrado la pelicula"})
 
 # put con el esquema
 @app.put('/movies/{id}', tags=['movies'])
@@ -148,71 +111,13 @@ def update_movie(id: int, movie: Movie):
             item['rating'] = movie.rating
             item['category'] = movie.category
             return movies
+    raise HTTPException(status_code=404, detail="Movie not found")
         
-# #Otra forma de hacer el update
-# @app.put('/movies/{id}', tags=['movies'])
-# async def update_movie(id: int, movie: Movie):
-
-#     for index, item in enumerate(movies):
-#         if item["id"] == id:
-#             movies[index].update(movie)
-#             movies[index]["id"] = id
-#             return movies[index]
-
 #delete
 @app.delete('/movies/{id}', tags=['movies'])
 def delete_movie(id:int):
     for movie in movies:
         if movie['id']==id:
             movies.remove(movie)
-            return movies
-
-# #put
-# @app.put('/movies/{id}', tags=['movies'])
-# async def update_movie(id: int, request: Request):
-#     update_movie = await request.json()
-#     for index, item in enumerate(movies):
-#         if item["id"] == id:
-#             movies[index].update(update_movie)
-#             return movies[index]
-
-#     raise HTTPException(status_code=404, detail="Movie not found")
-
-# #delete
-# @app.delete('/movies/{id}', tags=['movies'])
-# async def delete_movie(id: int):
-#     for index, item in enumerate(movies):
-#         if item["id"] == id:
-#             del movies[index]
-#             return {'status': 'deleted movie'}
-
-#     raise HTTPException(status_code=404, detail="Movie not found")
-
-
-
-# JSONResponse
-
-@app.get('/moviesJSON', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
-def get_movies2() -> List[Movie]:
-    return JSONResponse(status_code=200,content=movies )
-
-
-@app.get('/moviesJSON/{id}', tags=['movies'], response_model=Movie, status_code=200)
-def get_movie2(id: int = Path(ge=1, le=10000)) -> Movie:
-    for movie in movies:
-        if movie['id'] == id:
-            return JSONResponse(status_code=200, content=movie)
-    return JSONResponse(status_code=404, content=[])
-
-# parametros Query ... clave y valor
-@app.get('/moviesJSON/', tags=['movies'])
-def get_movies_by_category2(category: str = Query(min_length=5, max_length=15),status_code = 200):
-    data = [movie for movie in movies if movie['category'] == category]
-    return JSONResponse(status_code=200, content=data)
-
-
-@app.post('/moviesJSON', tags=['movies'], response_model=dict, status_code=201)
-# def create_movie(movie: Movie): #funciona igual
-def create_movie2(movie: Movie) -> dict:
-    movies.append(movie)
-    return JSONResponse(status_code=201, content={"message": "Se ha registrado la pelicula"})
+            return {'status': 'deleted movie'}
+    raise HTTPException(status_code=404, detail="Movie not found")
