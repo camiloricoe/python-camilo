432a0db0a0f730fc56f945e2c72fbb5c5a00f08d
camiloricoe <camilo.rico.echeverry@gmail.com>
2023-01-24 17:51:29 -0500

    post movie service

--- Platzi/FastAPI 1.0/services/movie.py
+++ Platzi/FastAPI 1.0/services/movie.py
@@ -1,4 +1,5 @@
 from models.movie import Movie as MovieModel
+from schemas.movie import Movie
 
 class MovieService():
     def __init__(self, db) -> None:
@@ -15,3 +16,9 @@ class MovieService():
     def get_movies_by_category(self,category):
         result=self.db.query(MovieModel).filter(MovieModel.category == category).all()
         return result
+    
+    def create_movie(self, movie: Movie):
+        new_movie=MovieModel(**movie.dict())
+        self.db.add(new_movie)
+        self.db.commit()
+        return 
