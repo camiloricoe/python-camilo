cf160e2c3f1e0961cba72e035e2555031b7feaac
camiloricoe <camilo.rico.echeverry@gmail.com>
2023-02-03 09:32:46 -0500

    Complexity of Comprehension

---
+++ CodeSignal/Arcade/python-arcade/5 Complexity of Comprehension/32-Word Power.py
@@ -0,0 +1,3 @@
+def solution(word):
+    num = {char: ord(char)-96 for char in word}
+    return sum([num[ch] for ch in word])
---
+++ CodeSignal/Arcade/python-arcade/5 Complexity of Comprehension/34-Multiplication Table.py
@@ -0,0 +1,2 @@
+def solution(n):
+    return [[element*ele for element in range(1,n+1)] for ele in range(1,n+1)]
